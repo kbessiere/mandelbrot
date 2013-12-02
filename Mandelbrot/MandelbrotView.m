89eeb37c2efb463f68568200f99a1533f54d442e
Kévin Bessière <bessiere.kevin@gmail.com>
2013-12-02 23:24:37 +0100

    Pinch rotation

--- Mandelbrot/MandelbrotView.m
+++ Mandelbrot/MandelbrotView.m
@@ -13,6 +13,9 @@
     unsigned int * buffer;
     CGRect _location;
     CGPoint _translate;
+    CGRect _originalScale;
+    unsigned int _maxIteration;
+    bool _isCalculating;
 }
 
 @end
@@ -30,15 +33,19 @@
 
 -(void)awakeFromNib
 {
+    UITapGestureRecognizer * tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)];
+    tapGesture.numberOfTapsRequired = 2;
     [self addGestureRecognizer:[[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchGesture:)]];
     [self addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)]];
-    [self addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)]];
+    [self addGestureRecognizer:tapGesture];
     buffer = malloc(self.frame.size.width * self.frame.size.height * sizeof(unsigned int));
     _location.origin.x = -2.5;
     _location.origin.y = -2;
     _location.size.width = 3;
     _location.size.height = self.frame.size.height* 3 / self.frame.size.width;
     _translate = CGPointZero;
+    _maxIteration = 100;
+    _isCalculating = NO;
 }
 
 
@@ -47,9 +54,13 @@
 - (void)handlePinchGesture:(UIGestureRecognizer *)aGestureRegonizer
 {
     UIPinchGestureRecognizer * pinchGesture = (UIPinchGestureRecognizer *)aGestureRegonizer;
-    NSLog(@"scale %f", pinchGesture.scale);
-    [self zoomWithScale:pinchGesture.scale atPoint:[pinchGesture locationInView:self]];
-    [self setNeedsDisplay];
+    if (pinchGesture.state == UIGestureRecognizerStateBegan)
+        _originalScale = _location;
+    else if (pinchGesture.state == UIGestureRecognizerStateChanged)
+    {
+        [self zoomWithScale:pinchGesture.scale atPoint:[pinchGesture locationInView:self]];
+        [self setNeedsDisplay];
+    }
 }
 
 - (void)handlePanGesture:(UIGestureRecognizer *)aGestureRecognizer
@@ -62,7 +73,7 @@
 - (void)handleTapGesture:(UIGestureRecognizer *)aGestureRecognizer
 {
     CGPoint point = [aGestureRecognizer locationInView:self];
-    [self zoomWithScale:1.2 atPoint:point];
+    [self zoomWithScale:1.20 atPoint:point];
     [self setNeedsDisplay];
 }
 
@@ -78,27 +89,29 @@
 - (void)zoomWithScale:(CGFloat)scale atPoint:(CGPoint)originPoint
 {
     CGRect zoomRect;
-    zoomRect.size.height = _location.size.height / scale;
-    zoomRect.size.width  = _location.size.width / scale;
+    zoomRect.size.height = _originalScale.size.height / scale;
+    zoomRect.size.width  = _originalScale.size.width / scale;
 
-    zoomRect.origin = _location.origin;
-    CGPoint coordinateMandelbrot = CGPointMake(originPoint.x * _location.size.width / self.frame.size.width,
-                                               originPoint.y * _location.size.height / self.frame.size.height);
-    zoomRect.origin.x = _location.origin.x - coordinateMandelbrot.x - (zoomRect.size.width / 2);
-    zoomRect.origin.y = _location.origin.y - (coordinateMandelbrot.y - (zoomRect.size.height / 2));
+    CGPoint coordinateMandelbrot = CGPointMake(originPoint.x * _originalScale.size.width / self.frame.size.width + _originalScale.origin.x,
+                                               originPoint.y * _originalScale.size.height / self.frame.size.height + _originalScale.origin.y);
+    zoomRect.origin.x = coordinateMandelbrot.x - (zoomRect.size.width * (coordinateMandelbrot.x - _originalScale.origin.x) / _originalScale.size.width);
+    zoomRect.origin.y = coordinateMandelbrot.y - (zoomRect.size.height * (coordinateMandelbrot.y - _originalScale.origin.y) / _originalScale.size.height);
     _location = zoomRect;
 }
 
 - (void)drawRect:(CGRect)rect
 {
+    if (_isCalculating == YES)
+        return;
+    _isCalculating = YES;
     CGContextRef context = UIGraphicsGetCurrentContext();
     unsigned int width = self.frame.size.width;
     unsigned int height = self.frame.size.height;
     
     float dX = _location.size.width / width;
     float dY = _location.size.height / height;
+    double time = CFAbsoluteTimeGetCurrent();
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    
     dispatch_apply(height, queue, ^(size_t j) {
         for (int i = 0; i < width; i++)
         {
@@ -107,7 +120,7 @@
             float a = x;
             float b = y;
             int iteration = 0;
-            while (a*a + b*b < 2*2 && iteration < 50)
+            while (a*a + b*b < 2*2 && iteration < _maxIteration)
             {
                 float aa = a * a;
                 float bb = b * b;
@@ -116,12 +129,13 @@
                 b = twoab + y;
                 iteration = iteration + 1;
             }
-            if (iteration == 50)
+            if (iteration == _maxIteration)
                 buffer[i + j * (unsigned int)self.frame.size.width] = 0;
             else
                 buffer[i + j * (unsigned int)self.frame.size.width] = iteration * 8;
         }
     });
+    NSLog(@"Take %f m",CFAbsoluteTimeGetCurrent()- time);
     
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     CGContextRef bitmapContext = CGBitmapContextCreate(
@@ -139,6 +153,7 @@
     CGSize viewSize = self.bounds.size;
     CGContextDrawImage(context, CGRectMake(0, 0, viewSize.width, viewSize.height), image);
     CGImageRelease(image);
+    _isCalculating = NO;
 }
          
 @end
